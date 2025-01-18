dcb5dff03262f7d23ccca6f6ac38c646e2ef460b
alianabil <alianabila3772@gmail.com>
2025-01-18 12:31:51 +0700

    kop

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
-        Documents techmart
+         Techmart
       </h1>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-      Techmart adalah aplikasi belanja online khusus untuk produk elektronik yang dirancang untuk memberikan pengalaman belanja yang mudah, aman, dan nyaman bagi pengguna.
+      Techmart adalah aplikasi belanja online yang menyediakan berbagai produk dari berbagai kategori untuk memenuhi kebutuhan sehari-hari. Aplikasi ini dirancang untuk memberikan pengalaman belanja yang mudah, aman, dan nyaman bagi pengguna.
       </p>
       <div className="flex items-center gap-5">
         <Link
